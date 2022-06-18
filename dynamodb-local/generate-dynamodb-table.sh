e9e5a41684da63b0086e427abfb2f33d8c65772e
ruritoBlogger <ruriro0125@gmail.com>
2022-06-18 22:54:38 +0900

    fix: dynamodb-localに関係するシェルファイルなどの置き場所を変更した

--- dynamodb-local/generate-dynamodb-table.sh
+++ dynamodb-local/generate-dynamodb-table.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-aws dynamodb create-table --cli-input-json file://localstack/dynamodb.json --endpoint-url http://0.0.0.0:8000 \
+aws dynamodb create-table --cli-input-json file://dynamodb-local/dynamodb.json --endpoint-url http://0.0.0.0:8000 \
 && aws dynamodb list-tables --endpoint-url http://0.0.0.0:8000
