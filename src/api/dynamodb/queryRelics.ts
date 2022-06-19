48c52c41ee1b106cbd1baa9c712d3a56521a037a
ruritoBlogger <ruriro0125@gmail.com>
2022-06-19 15:32:17 +0900

    refactor: prettierが走っていなかったので反映させた

--- src/api/dynamodb/queryRelics.ts
+++ src/api/dynamodb/queryRelics.ts
@@ -1,24 +1,24 @@
-import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb"
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
 
 interface queryProps {
-    user_id: number
+  user_id: number;
 }
 
 export const queryRelics = async ({ user_id }: queryProps) => {
-    const dynamoClient = new DynamoDBClient({
-        endpoint: "http://localhost:8000",
-        region: 'ap-northeast-1',
-        credentials: { accessKeyId: 'dummy', secretAccessKey: 'dummy' }
-    })
+  const dynamoClient = new DynamoDBClient({
+    endpoint: "http://localhost:8000",
+    region: "ap-northeast-1",
+    credentials: { accessKeyId: "dummy", secretAccessKey: "dummy" },
+  });
 
-    // TODO: テーブル名などを決め打ちしたくない
-    const command = new QueryCommand({
-        TableName: 'relics',
-        KeyConditionExpression: "user_id = :user_id",
-        ExpressionAttributeValues: {
-            ":user_id": { N: String(user_id) }
-        },
-    })
+  // TODO: テーブル名などを決め打ちしたくない
+  const command = new QueryCommand({
+    TableName: "relics",
+    KeyConditionExpression: "user_id = :user_id",
+    ExpressionAttributeValues: {
+      ":user_id": { N: String(user_id) },
+    },
+  });
 
-    return await dynamoClient.send(command)
-}
+  return await dynamoClient.send(command);
+};
