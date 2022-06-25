6b13592c8c865dcc886a58f9bbb74d4e28eb48bf
ruritoBlogger <ruriro0125@gmail.com>
2022-06-25 16:32:35 +0900

    feat: Record型で管理していた聖遺物の型情報をより厳密にした

--- src/domains/relic.ts
+++ src/domains/relic.ts
@@ -1,5 +1,8 @@
 // # TODO: DBの設計をしておく
-export type Relic = Record<"user_id" | "name", string>;
+export type Relic = {
+  user_id: number;
+  name: string;
+};
 
 export function isRelic(target: Record<string, any>): target is Relic {
   return target.user_id !== undefined && target.name !== undefined;
