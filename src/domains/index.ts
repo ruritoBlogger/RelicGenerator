1bdb4610413f3585e9d11c194d429a74226e224c
ruritoBlogger <ruriro0125@gmail.com>
2022-06-25 15:58:02 +0900

    feat: domains dirにindex.tsを設置してimportしやすいようにした

---
+++ src/domains/index.ts
@@ -0,0 +1,7 @@
+import type { Relic } from "./relic";
+import { isRelic } from "./relic";
+import type { RelicNames } from "./relicNames";
+import type { subParameter } from "./subParameter";
+
+export type { Relic, RelicNames, subParameter };
+export { isRelic };
