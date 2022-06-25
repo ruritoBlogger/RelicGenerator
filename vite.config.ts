937c17105fe85f8512469c1bcdc8df4ce479aeff
ruritoBlogger <ruriro0125@gmail.com>
2022-06-26 01:26:01 +0900

    chore: tsconfigのpathsをトランスパイル後も反映させるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), tsconfigPaths()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
   },
   define: {
-    'process.env': {},
+    "process.env": {},
     global: {},
   },
 });
