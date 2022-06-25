02be16b25d05f9237e42e81c0001b2a42d9fcafc
ruritoBlogger <ruriro0125@gmail.com>
2022-06-26 00:43:15 +0900

    feat: subParameter Typeを改良して型ガード関数をはやした

--- src/domains/subParameter.ts
+++ src/domains/subParameter.ts
@@ -1,35 +1,69 @@
 export type subParameter =
-  | HPParameter
-  | AttackParameter
-  | DefenseParameter
-  | FamiliarParameter
-  | ChargeParameter
-  | ConfessionRateParameter
-  | ConfessionDamageParameter
-  | MedicalParameter
-  | DamageBuffParameter;
+  | HPParameterType
+  | AttackParameterType
+  | DefenseParameterType
+  | FamiliarParameterType
+  | ChargeParameterType
+  | ConfessionRateParameterType
+  | ConfessionDamageParameterType
+  | MedicalParameterType
+  | DamageBuffParameterType;
 
-// HP | HP(%)
-type HPParameter = "HP" | "HP_rate";
-// 攻撃力 | 攻撃力(%)
-type AttackParameter = "Attack" | "Attack_rate";
-// 防御力 | 防御力(%)
-type DefenseParameter = "Defense" | "Defense_rate";
+// HP, HP(%)
+const HPParameters = ["HP", "HP_rate"];
+type HPParameterType = typeof HPParameters[number];
+
+// 攻撃力, 攻撃力(%)
+const AttackParameters = ["Attack", "Attack_rate"];
+type AttackParameterType = typeof AttackParameters[number];
+
+// 防御力, 防御力(%)
+const DefenseParameters = ["Defense", "Defense_rate"];
+type DefenseParameterType = typeof DefenseParameters[number];
+
 // 元素熟知
-type FamiliarParameter = "Familiar";
+const FamiliarParameter = "Familiar";
+type FamiliarParameterType = typeof FamiliarParameter;
+
 // 元素チャージ効率(%)
-type ChargeParameter = "Charge_rate";
+const ChargeParameter = "Charge_rate";
+type ChargeParameterType = typeof ChargeParameter;
+
 // 会心率(%)
-type ConfessionRateParameter = "Confession_rate";
+const ConfessionRateParameter = "Confession_rate";
+type ConfessionRateParameterType = typeof ConfessionRateParameter;
+
 // 会心ダメージ(%)
-type ConfessionDamageParameter = "ConfessionDamage_rate";
+const ConfessionDamageParameter = "ConfessionDamage_rate";
+type ConfessionDamageParameterType = typeof ConfessionDamageParameter;
+
 // 治療効果(%)
-type MedicalParameter = "Medical_rate";
-// 各種ダメージバフ(%), 物理ダメージ | 炎元素ダメージ | 水元素ダメージ | 氷元素ダメージ | 雷元素ダメージ | 岩元素ダメージ
-type DamageBuffParameter =
-  | "PhysicsDamage_rate"
-  | "FireDamage_rate"
-  | "WaterDamage_rate"
-  | "IceDamage_rate"
-  | "ThunderDamage_rate"
-  | "RockDamage_rate";
+const MedicalParameter = "Medical_rate";
+type MedicalParameterType = typeof MedicalParameter;
+
+// 各種ダメージバフ(%): 物理ダメージ, 炎元素ダメージ, 水元素ダメージ, 氷元素ダメージ, 雷元素ダメージ, 岩元素ダメージ
+const DamageBuffParameters = [
+  "PhysicsDamage_rate",
+  "FireDamage_rate",
+  "WaterDamage_rate",
+  "IceDamage_rate",
+  "ThunderDamage_rate",
+  "RockDamage_rate",
+];
+type DamageBuffParameterType = typeof DamageBuffParameters[number];
+
+// FIXME: つらいやり方をしてしまった
+export function isSubParameter(target: unknown): target is subParameter {
+  return (
+    typeof target === "string" &&
+    (target in HPParameters ||
+      target in AttackParameters ||
+      target in DefenseParameters ||
+      target === FamiliarParameter ||
+      target === ChargeParameter ||
+      target === ConfessionRateParameter ||
+      target === ConfessionDamageParameter ||
+      target === MedicalParameter ||
+      target in DamageBuffParameters)
+  );
+}
