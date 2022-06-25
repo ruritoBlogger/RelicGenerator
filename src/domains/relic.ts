89bd891ce7e5e14a93ea766ac3b0b74ab947c572
ruritoBlogger <ruriro0125@gmail.com>
2022-06-25 16:50:54 +0900

    feat: Relic型にRelicType型を追加した

--- src/domains/relic.ts
+++ src/domains/relic.ts
@@ -1,9 +1,28 @@
+import { RelicName, RelicNames } from "@domains/relicNames";
+
 // # TODO: DBの設計をしておく
 export type Relic = {
   user_id: number;
   name: string;
+  relicType: RelicName;
 };
 
-export function isRelic(target: Record<string, any>): target is Relic {
-  return target.user_id !== undefined && target.name !== undefined;
+export function isRelic(maybeRelic: Record<string, any>): maybeRelic is Relic {
+  return (
+    isNum(maybeRelic.user_id) &&
+    isStr(maybeRelic.name) &&
+    hasRelicType(maybeRelic.relicType)
+  );
+}
+
+function isNum(target: unknown): target is number {
+  return target !== undefined && typeof target === "number";
+}
+
+function isStr(target: unknown): target is string {
+  return target !== undefined && typeof target === "string";
+}
+
+function hasRelicType(target: unknown): target is RelicName {
+  return isStr(target) && RelicNames.includes(target);
 }
