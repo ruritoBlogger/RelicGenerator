6641fe1cad9289c1f0d635842f433d8b30423fd1
ruritoBlogger <ruriro0125@gmail.com>
2022-07-17 18:44:34 +0900

    fix: DynamoDBのurlの指定方法を改善

--- dynamodb-local/generate-dynamodb-table.sh
+++ dynamodb-local/generate-dynamodb-table.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-aws dynamodb create-table --cli-input-json file://dynamodb-local/dynamodb.json --endpoint-url http://0.0.0.0:8000 \
-&& aws dynamodb list-tables --endpoint-url http://0.0.0.0:8000
+aws dynamodb create-table --cli-input-json file://dynamodb-local/dynamodb.json --endpoint-url http://localhost:8000 \
+&& aws dynamodb list-tables --endpoint-url http://localhost:8000
