9ceb11573a8b3be11ef79d85805dc43cf968c2be
ruritoBlogger <ruriro0125@gmail.com>
2022-07-17 20:04:41 +0900

    feat: DynamoDBにデータを置くカスタムフックをDBのテーブル構造に合わせて改良

--- src/api/dynamodb/putRelic.ts
+++ src/api/dynamodb/putRelic.ts
@@ -1,23 +1,61 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { Relic } from "@domains/relic";
 
-interface putProps {
-  name: string;
-}
-
-export const putRelic = async ({ name }: putProps) => {
+export const putRelic = async ({
+  name,
+  relicType,
+  subParameters,
+}: Omit<Relic, "created_at" | "user_id">) => {
   const dynamoClient = new DynamoDBClient({
     endpoint: "http://localhost:8000",
     region: "ap-northeast-1",
     credentials: { accessKeyId: "dummy", secretAccessKey: "dummy" },
   });
 
-  // TODO: tableをコード側で生成する
+  /**
+   * 置きたい情報
+   * {
+   *   "HogeParam": value,
+   *   "FugaParam": value,
+   * }
+   *
+   * in AttributeValue
+   * M: {
+   *    "HogeParam": { "N": value },
+   *    "FugaParam": { "N": value }
+   * }
+   *
+   * subParametersの中身
+   * [
+   *   {
+   *     "name": "HogeParam",
+   *     "value": value,
+   *   },
+   *   {
+   *     "name": "FugaParam",
+   *     "value": value
+   *   }
+   * ]
+   */
+  const mapValue = {};
+  subParameters.forEach((subParameter) => {
+    Object.assign(mapValue, {
+      [subParameter.name]: { N: subParameter.value },
+    });
+  });
+
   // TODO: テーブル名などを決め打ちしたくない
   const command = new PutItemCommand({
     TableName: "relics",
     Item: {
+      // NOTE: ユーザーは今のところID: 1固定
       user_id: { N: "1" },
+      created_at: { S: new Date().toString() },
       name: { S: name },
+      relicType: { S: relicType },
+      subParameters: {
+        M: mapValue,
+      },
     },
   });
 
