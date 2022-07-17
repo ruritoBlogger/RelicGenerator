5d208a6a9dafa25cabc327cf98301c1bee4bb120
ruritoBlogger <ruriro0125@gmail.com>
2022-07-17 18:35:54 +0900

    feat: 聖遺物データのダミーを作成するコンポーネントのボタンとカスタムフックを紐づけた

--- src/components/FixtureDataGenerator/index.tsx
+++ src/components/FixtureDataGenerator/index.tsx
@@ -1,15 +1,21 @@
 import { Component } from "solid-js";
+import { usePutRelic } from "@hooks/usePutRelic";
 
 /**
  * DBに検証用の聖遺物データを追加するコンポーネント
  * TODO: 生成するデータ数とかを呼び出し元で制御出来るようにしたい
  */
 const FixtureDataGenerator: Component = () => {
-  const handleClick = () => {
-    console.log("clicked");
+  const { isLoading, putRelic } = usePutRelic();
+  const handleClick = async () => {
+    await putRelic();
   };
 
-  return <button onClick={handleClick}>テストデータを生成する</button>;
+  return (
+    <button disabled={isLoading()} onClick={handleClick}>
+      テストデータを生成する
+    </button>
+  );
 };
 
 export default FixtureDataGenerator;
