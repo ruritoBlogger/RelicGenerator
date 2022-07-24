5406aa04e845b78a95ceb618db4b98a8cd9131d9
ruritoBlogger <ruriro0125@gmail.com>
2022-07-24 13:24:48 +0900

    feat: 聖遺物を作成するカスタムフック内部にて決め打ちしているデータを改良した

--- src/hooks/usePutRelic.ts
+++ src/hooks/usePutRelic.ts
@@ -12,7 +12,24 @@ export const usePutRelic = (): UsePutRelicResult => {
 
   const callback = async () => {
     setIsLoading(true);
-    await putRelic({ name: "testaaa1234" });
+    await putRelic({
+      name: "そこそこ強いやつ",
+      relicType: "逆飛びの流星",
+      subParameters: [
+        {
+          name: "HP_rate",
+          value: 7.8,
+        },
+        {
+          name: "Attack_rate",
+          value: 17.8,
+        },
+        {
+          name: "Confession_rate",
+          value: 27.8,
+        },
+      ],
+    });
     setIsLoading(false);
   };
 
