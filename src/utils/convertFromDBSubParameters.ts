a8665e5ad7c941aac59f29e07a5953b20fca3816
ruritoBlogger <ruriro0125@gmail.com>
2022-07-30 19:29:54 +0900

    fix: DBから取得したデータを加工する部分がバグっていたので修正

---
+++ src/utils/convertFromDBSubParameters.ts
@@ -0,0 +1,35 @@
+import { subParameter, isSubParameter } from "@domains/subParameter";
+
+/**
+ * DynamoDBから取得したサブパラメーターを正しい形式に変換する
+ * ex
+ *   { HP_rate: 3.0, RockDamage_rate: 10.2 } => [{ name: HP_rate, value: 3.0 }, { name: RockDamage_rate, value: 10.2 }]
+ * @param output DynamoDBから取得したサブパラメーター
+ * @return 整形済みのサブパラメーター
+ */
+export const convertFromDBSubParameters = (
+  output: unknown
+): { name: subParameter; value: number }[] | null => {
+  if (output === null || typeof output !== "object") return null;
+  if (!allHaveSubParameterKeys(output)) return null;
+
+  const keys = Object.keys(output);
+
+  if (keys.every((key) => typeof output[key] === "number")) {
+    return keys.map((key) => ({ name: key, value: output[key] }));
+  } else {
+    return null;
+  }
+};
+
+// TODO: この処理は別で管理したい
+function allHaveSubParameterKeys(
+  target: unknown
+): target is { [name: subParameter]: any } {
+  if (target === null || typeof target !== "object") return false;
+
+  const keys = Object.keys(target);
+  if (keys.some((key) => !isSubParameter(key))) return false;
+
+  return true;
+}
--- src/utils/convertFromQueryOutput.ts
+++ src/utils/convertFromQueryOutput.ts
@@ -1,6 +1,7 @@
 import { QueryCommandOutput } from "@aws-sdk/client-dynamodb";
 import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { isRelic, Relic } from "@domains/relic";
+import { convertFromDBSubParameters } from "./convertFromDBSubParameters";
 
 /**
  * DynamoDBのQuery結果から必要な情報(今回だと聖遺物の情報)に変換する
@@ -12,8 +13,16 @@ export const convertToRelics = (output: QueryCommandOutput): Array<Relic> => {
     return [];
   } else {
     const items = rawItems.map((item) => unmarshall(item));
-    // NOTE: 日付データ(String) -> 日付データ(Date)
-    items.map((item) => ({ ...item, created_at: new Date(item.created_at) }));
-    return items.filter<Relic>((item): item is Relic => isRelic(item));
+    /**
+     * NOTE: 日付データ(String) -> 日付データ(Date)
+     *       サブパラメーター({key: value}) -> サブパラメーター({name: key, value: value})
+     */
+    const convertedItems = items.map((item) => ({
+      ...item,
+      created_at: new Date(item.created_at),
+      subParameters: convertFromDBSubParameters(item.subParameters),
+    }));
+    console.log(convertedItems);
+    return convertedItems.filter<Relic>((item): item is Relic => isRelic(item));
   }
 };
