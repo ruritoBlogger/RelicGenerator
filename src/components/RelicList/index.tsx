a7ee8aa4384f18e5fd30afb49bf53df2db29d252
ruritoBlogger <ruriro0125@gmail.com>
2022-08-13 17:26:58 +0900

    feat: 一覧ページのUIを簡単に整えた

--- src/components/RelicList/index.tsx
+++ src/components/RelicList/index.tsx
@@ -1,4 +1,12 @@
 import { Relic } from "@domains/relic";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 interface RelicListProps {
   relicList: Array<Relic>;
@@ -6,18 +14,32 @@ interface RelicListProps {
 
 export const RelicList = ({ relicList }: RelicListProps): JSX.Element => {
   return (
-    <>
-      {relicList.map((item) => (
-        <div key={item.created_at.getUTCSeconds()}>
-          <p>{item.name}</p>
-          <p>{item.relicType}</p>
-          {item.subParameters.map((subParameter) => (
-            <p key={subParameter.name}>
-              {subParameter.name}: {subParameter.value}
-            </p>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>聖遺物名</TableCell>
+            <TableCell>種類</TableCell>
+            <TableCell>サブパラメーター(1)</TableCell>
+            <TableCell>サブパラメーター(2)</TableCell>
+            <TableCell>サブパラメーター(3)</TableCell>
+            <TableCell>サブパラメーター(4)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {relicList.map((item) => (
+            <TableRow key={item.created_at.getUTCSeconds()}>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.relicType}</TableCell>
+              {item.subParameters.map((subParameter) => (
+                <TableCell key={subParameter.name}>
+                  {subParameter.name}: {subParameter.value}
+                </TableCell>
+              ))}
+            </TableRow>
           ))}
-        </div>
-      ))}
-    </>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
