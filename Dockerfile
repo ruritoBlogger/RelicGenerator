b93908416020179e8c876995a248aa336ae7c06a
ruritoBlogger <ruriro0125@gmail.com>
2022-08-17 14:46:27 +0900

    chore: SSRする関係上APIのendpointとの接続が困難になるため、フロントをコンテナで動作させないようにした

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:16
-
-RUN apt-get update -qq
-
-RUN mkdir /app
-WORKDIR /app
-COPY . .
--- src/db/config.ts
+++ src/db/config.ts
@@ -2,7 +2,7 @@ import { DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 
 export const config: DynamoDBClientConfig = {
   // NOTE: SSRしている関係上サーバー側でもクライアント側でも呼び出される
-  endpoint: "http://0.0.0.0:8000",
+  endpoint: "http://localhost:8000",
   region: "ap-northeast-1",
   credentials: {
     accessKeyId: "dummy",
